38f2b9b3e3957c4252738db004cc307cc3ab1edb
ThomasSte01 <thomas.stevens3@student.howest.be>
2025-04-24 15:16:03 +0200

    modified pyproject.toml so that non code files also get included when making a site-package

--- multiqc_cmgg/templates/cmgg/__init__.py
+++ multiqc_cmgg/templates/cmgg/__init__.py
@@ -9,10 +9,10 @@ CMGG Custom report template
 """
 import os
 
-template_dir = os.path.dirname(__file__)
+template_dir =os.path.dirname(__file__)
 template_parent = "default"
 base_fn = "base.html"
 
 import logging
 logging.info("Custom template being loaded")
-logging.info(f"path template{template_dir}")
+logging.info(f"path template: {template_dir}")
--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 The project is configured in pyproject.toml. This script is left for the editable installation.
 """
 
-from setuptools import setup  # type: ignore
+from setuptools import setup,find_packages  # type: ignore
 
-setup()
+setup(
+    name='multiqc_cmgg',
+    packages=find_packages(),
+    include_package_data=True,
+)
