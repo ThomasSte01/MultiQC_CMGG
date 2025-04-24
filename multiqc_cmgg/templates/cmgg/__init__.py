b664ee88237a29c38ae7d9dc658aecde55240c9f
ThomasSte01 <thomas.stevens3@student.howest.be>
2025-04-24 13:26:09 +0200

    added default template to pyproject.toml and changed template

--- multiqc_cmgg/templates/cmgg/__init__.py
+++ multiqc_cmgg/templates/cmgg/__init__.py
@@ -12,3 +12,7 @@ import os
 template_dir = os.path.dirname(__file__)
 template_parent = "default"
 base_fn = "base.html"
+
+import logging
+logging.info("Custom template being loaded")
+logging.info(f"path template{template_dir}")
